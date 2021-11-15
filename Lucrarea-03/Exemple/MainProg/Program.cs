bc825bf07e789b4750c185e6662c9271601af5b3
AndreeaBradea <andreea.bradea@yahoo.com>
2021-11-15 17:35:17 +0200

    Mesaj

---
+++ Lucrarea-03/Exemple/MainProg/Program.cs
@@ -0,0 +1,88 @@
+﻿using Exemple.Domain.Models;
+using System;
+using System.Collections.Generic;
+using static Exemple.Domain.Models.Carucior;
+using static Exemple.Domain.PriceOperations;
+using Exemple.Domain;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using System.Net.Http;
+
+namespace MainProgram
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+
+        static async Task Main(string[] args)
+        {
+            var listOfProducts = ReadListOfProducts().ToArray();
+            PublishQuantityCommand command = new(listOfProducts);
+            PublishProductWorkflow workflow = new();
+            var result = await workflow.ExecuteAsync(command, CheckProductExists);
+
+
+            result.Match(
+                    whenPaidCaruciorFaildEvent: @event =>
+                    {
+                        Console.WriteLine($"Publish failed: {@event.Reason}");
+                        return @event;
+                    },
+                    whenPaidCaruciorScucceededEvent: @event =>
+                    {
+                        Console.WriteLine($"Publish succeeded.");
+                        return @event;
+                    }
+                );
+
+            Console.WriteLine("END");
+        }
+
+        private static List<UnvalidatedProductQuantity> ReadListOfProducts()
+        {
+            List<UnvalidatedProductQuantity> listOfProducts = new();
+            string flag = "N";
+            do
+            {
+                var productCode = ReadValue("Cod produs: ");
+                while (string.IsNullOrEmpty(productCode))
+                {
+                    productCode = ReadValue("Codul nu trebuie sa fie vid: ");
+                }
+
+                var quantity = ReadValue("Cantitate: ");
+                while (string.IsNullOrEmpty(quantity))
+                {
+                    quantity = ReadValue("Cantitatea nu trebuie sa fie vida: ");
+                }
+
+                var address = ReadValue("Adresa: ");
+                while (string.IsNullOrEmpty(address))
+                {
+                    address = ReadValue("Adresa nu trebuie sa fie vida: ");
+                }
+
+                listOfProducts.Add(new(productCode, quantity, address));
+                flag = ReadValue("Adaugati si alte produse? [Y/N] ");
+            } while (flag != "N");
+            return listOfProducts;
+        }
+
+        private static string? ReadValue(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        private static TryAsync<bool> CheckProductExists(ProductCode product)
+        {
+            Func<Task<bool>> func = async () =>
+            {
+
+                return true;
+            };
+            return TryAsync(func);
+        }
+    }
+}
